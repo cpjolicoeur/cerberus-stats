f0c0e787721fcabebfb8d13605bc38f9e7e62922
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2010-04-25 16:49:50 -0400

    only grab element contents

--- retrieve_stats.rb
+++ retrieve_stats.rb
@@ -5,14 +5,14 @@ require 'open-uri'
 
 doc = Nokogiri::HTML( open( 'http://rubygems.org/gems/cerberus' ) )
 
-doc.css( 'h3' ).each do |h3|
-  puts "version: #{h3}"
+doc.css( 'h3' ).each do |elm|
+  puts "version: #{elm.content}"
 end
 
 doc.css( 'span:nth-child(1) strong' ).each do |elm|
-  puts "total downloads: #{elm}"
+  puts "total downloads: #{elm.content}"
 end
 
 doc.css( 'span:nth-child(2) strong' ).each do |elm|
-  puts "version downloads: #{elm}"
+  puts "version downloads: #{elm.content}"
 end
